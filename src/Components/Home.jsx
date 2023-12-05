c19de92d1b67b480790d5b1bd10017e59ebfe9a2
Dew2003 <152517848+Dew2003@users.noreply.github.com>
2023-12-05 06:28:10 +0000

    img changed

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,9 +20,8 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
-
-const imageAltText = "picture";
+import image from "../images/mosaic.svg";
+const imageAltText = "purple and blue abstract background";
 
 const Home = ({ name, title }) => {
   return (
